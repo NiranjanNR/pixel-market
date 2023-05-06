7a20a44ec8cf762b5685d5ce4726c799ddddbe79
NiranjanNR <84635960+NiranjanNR@users.noreply.github.com>
2023-05-06 17:24:53 +0530

    added firebase

--- src/components/GridCard/GridCard.js
+++ src/components/GridCard/GridCard.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import Card from '../Card/Card'
+import { useEffect,useState } from 'react'
+import {db} from '../../firebase'
+import {
+    query,
+    collection,
+    onSnapshot,
+  } from 'firebase/firestore';
 
 const GridCard = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const q = query(collection(db, 'digital-art'));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+          let todosArr = [];
+          querySnapshot.forEach((doc) => {
+            todosArr.push({ ...doc.data(), id: doc.id });
+          });
+          setTodos(todosArr);
+        });
+        return () => unsubscribe();
+      }, []);
+
     return (
-        <div id="Cards" className='ml-6 overflow-x-scroll'>
-            <div className='flex flex-row gap-4'>
+        <div id="Cards" className='ml-6 overflow-x-scroll text-white'>
+            {/* <div className='flex flex-row gap-4'>
                 <div>
                     <Card image={"https://unft-marketplace.vercel.app/_next/image?url=https%3A%2F%2Fimgix.cosmicjs.com%2Fc3d63a90-fa2a-11ec-b458-99bc1b8dd3ef-card-pic-4.jpg&w=1920&q=60"} name={"Mood"} price={19} number={31} />
                 </div>
@@ -17,6 +39,13 @@ const GridCard = () => {
                 <div>
                     <Card image={"https://www.cnet.com/a/img/resize/e547a2e4388fcc5ab560f821ac170a59b9fb0143/hub/2021/12/13/d319cda7-1ddd-4855-ac55-9dcd9ce0f6eb/unnamed.png?auto=webp&fit=crop&height=1200&width=1200"} name={"Ape"} price={19} number={30} />
                 </div>
+            </div> */}
+            <div className='flex flex-row gap-4'>
+                {todos.map((todo) => (
+                    <div>
+                        <Card image={todo.image} name={todo.name} price={todo.price} number={todo.number} />
+                    </div>
+                ))}
             </div>
         </div>
     )
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAenYCe5Y9vG852k09lAu8cjJvKKJwCIs4",
+  authDomain: "pixel-market-87191.firebaseapp.com",
+  projectId: "pixel-market-87191",
+  storageBucket: "pixel-market-87191.appspot.com",
+  messagingSenderId: "913387788562",
+  appId: "1:913387788562:web:e05c92aecde3ace5233bcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
