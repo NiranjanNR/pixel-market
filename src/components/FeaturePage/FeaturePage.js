60f63fad110e2ec58cce82f1bc1ae61b4c77e996
Uday Krishnan <udaykrishnanvg@gmail.com>
2023-05-14 18:20:04 +0530

    Feature page functionality implementation

--- src/components/FeaturePage/FeaturePage.js
+++ src/components/FeaturePage/FeaturePage.js
@@ -1,31 +1,61 @@
-import React from "react";
+import { useEffect, useState } from 'react'
+import  db  from '../../firebase'
+import {
+    collection,
+    onSnapshot,getDocs
+} from 'firebase/firestore';
 
 const FeaturePage = () => {
- 
-    return (
-      <section className="px-12 bg-[#141416] my-40  ">
-        <div className="container flex gap-8 max-lg:flex-col ">
+    const [todos, setTodos] = useState([]);
+  
+    useEffect(() => {
+      const unsubscribe = onSnapshot(collection(db, "explore-digart"), (snapshot) => {
+        const todosArr = [];
+        snapshot.forEach((doc) => {
+          todosArr.push({ ...doc.data(), id: doc.id });
+        },[]);
+  
        
+        getDocs(collection(db, "digital-art")).then((digitalArtSnapshot) => {
+     
+          const filteredTodos = todosArr.filter((todo) => {
+            const imageExists = digitalArtSnapshot.docs.some(
+              (doc) => doc.data().image === todo.image
+            );
+            return !imageExists;
+          });
+  
+          setTodos(filteredTodos);
+        });
+      });
+  
+      return () => unsubscribe();
+    }, []);
+  
+    return (
+      <section className=" bg-[#141416] my-40  ">
+        <div className="container flex gap-8 max-lg:flex-col ">
+          {todos.slice(0, 1).map((todo) => (
             <>
               <div className="">
                 <img
                   className="lg:w-[490px] lg:h-[600px] w-[190px] h-[300px] object-cover rounded-xl mx-auto"
-                  src='https://ezway.s3.amazonaws.com/jondo/nft/nft-header-2.jpg'
-                  alt='hello'
+                  src={todo.image}
+                  alt={todo.name}
                 />
               </div>
               <div className="flex flex-col w-full gap-2 justify-center items-center">
-                <h2 className="text-4xl md:text-6xl text-[#FCFCFD] ">Chef Monkey</h2>
+                <h2 className="text-4xl md:text-6xl text-[#FCFCFD] mb-12">{todo.title}</h2>
                 <div className="flex gap-4 font-bold items-center">
                   <span className="p-1 text-[#45b26b] border-[#45b26b] border-[2px] rounded-[4px]">
-                    $15
+                    {todo.price}
                   </span>
                   <span className="text-base text-[#777E90]">
-                    45 in stock
+                    {todo.number} in stock
                   </span>
                 </div>
                 <div className="mt-6 text-base text-[#777E90] md:max-w-[80%]">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <p>{todo.description}</p>
                 </div>
                 <div className="flex justify-center">
   <button className="mt-8 w-32 h-12 px-6 text-bold inline-flex justify-center items-center bg-[#4437ff] text-white whitespace-nowrap">
@@ -35,7 +65,7 @@ const FeaturePage = () => {
 
               </div>
             </>
-         
+          ))}
         </div>
       </section>
     );
